d8e58ae53df08d28e2a4a48db87542d3ab488ba9
lombritz <jaimitorojas@gmail.com>
2014-12-30 16:29:03 -0400

    Optimize pending order screen load

--- addons/pos_ncf/static/src/js/widgets.js
+++ addons/pos_ncf/static/src/js/widgets.js
@@ -3,28 +3,18 @@ function openerp_pos_ncf_widgets(instance, module) { //module is instance.point_
     var _t = instance.web._t;
 
     module.PosWidget = module.PosWidget.extend({
-
-        start: function() {
-            var self = this;
-            this._super();
-
-
-        },
-
         // This method instantiates all the screens, widgets, etc. If you want to add new screens change the
         // startup screen, etc, override this method.
         build_widgets: function () {
             var self = this;
             this._super();
 
-            // --------  New Screens definitions.
-
-            this.orderlist_screen = new module.PendingOrderListScreenWidget(this, {});
-            this.orderlist_screen.appendTo(this.$('.screens'));
-
-            // -------- Misc.
-
             if (false) {// TODO: implement configurable parameter for the POS to allow unpaid orders.
+                // --------  New Screens definitions.
+                this.orderlist_screen = new module.PendingOrderListScreenWidget(this, {});
+                this.orderlist_screen.appendTo(this.$('.screens'));
+
+                // -------- Misc.
                 this.pendingorders_button = new module.HeaderButtonWidget(this, {
                     label: _t('Ordenes Pendientes'),
                     action: function () {
@@ -32,12 +22,10 @@ function openerp_pos_ncf_widgets(instance, module) { //module is instance.point_
                     }
                 });
                 this.pendingorders_button.appendTo(this.$('.pos-rightheader'));
+
+                // --------  Adding Pending Order Screen to screen_selector.
+                this.screen_selector.add_screen('orderlist', this.orderlist_screen);
             }
-
-            // --------  Adding Pending Order Screen to screen_selector.
-
-            this.screen_selector.add_screen('orderlist', this.orderlist_screen);
-
         }
     });
 }
